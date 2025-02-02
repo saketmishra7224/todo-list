0f9466163b5e6f8beced363f86d4b53e26f7a67b
Saket <Saket_mishra@gmail.com>
2025-02-02 18:53:36 +0530

    Added new components in each file

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <div>{12+4}</div>
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
